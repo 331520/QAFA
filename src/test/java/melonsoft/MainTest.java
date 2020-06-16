5e4d74622f54aef52976cbd6ff9ecbd25d778f71
33152 <tegeran43>
2020-06-16 22:30:19 +0300

    1. Init commit

---
+++ src/test/java/melonsoft/MainTest.java
@@ -0,0 +1,12 @@
+package test.java.melonsoft;
+
+import org.testng.annotations.Test;
+
+public class MainTest {
+
+    //test login to Siebel
+    @Test
+    public static void successLogin() {
+        System.out.println("Hello World");
+    }
+}
